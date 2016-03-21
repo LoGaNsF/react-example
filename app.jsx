30bdc915f2591eb7a05267057dc37a45416d208c
LoGaNsF <logansf@gmail.com>
2016-03-21 20:02:13 +0100

    Initial commit

---
+++ app.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Hello = require('./components/hello');
+
+ReactDOM.render(<Hello name="LoGaNsF" />, document.getElementById('app'));
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+var browserSync = require('browser-sync').create();
+var babelify = require('babelify');
+var browserify = require('browserify');
+var watchify = require('watchify');
+
+function bundle(bundler) {
+  return bundler.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
+}
+
+gulp.task('build', function () {
+    var bundler = browserify('./app.jsx', {
+        debug: true,
+        extensions: [".jsx"]
+    }).transform(babelify, {
+        presets: ["react"]
+    });
+        
+    bundle(bundler);        
+});
+
+gulp.task('watch', function () {
+    var bundler = watchify(
+        browserify('./app.jsx', {
+            debug: true,
+            extensions: [".jsx"]
+        }).transform(babelify, {
+            presets: ["react"]
+        })
+    );
+    
+    bundle(bundler)
+
+    bundler.on('update', function () {
+        bundle(bundler)
+    });
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: "./"
+    });
+});
+
+gulp.task('default', ['watch', 'serve']);
